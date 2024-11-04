50c990e05cf6cf7284644103aa04715a41863bf5
jqinEDA64 <jimmyqin@stanford.edu>
2024-11-04 12:40:11 -0800

    Split "Interface" class into a "Surface" class and an "Interface" class, where the "Surface" refers to a single material. Seems more modular/organized this way.`

--- vdW_distance.py
+++ vdW_distance.py
@@ -12,46 +12,82 @@ ANGSTROM_BOHR_CONST = 0.53 # [a0/A]
 EV_J_CONST = Q_CONST       # [J/eV]
 PI = np.pi                 # []
 
-
-# Interface class. Generic class which represents metal-semiconductor interfaces.
-# Computes quantities of interest, such as vdW gap distance.
-#
-# NOTE: in this class, the following units are used:
-# - Length: Angstrom [A]
-# - Energy: Hartree  [Ha]
-class Interface :
-    
-    # Constructor for metal-semiconductor interface
-    # Here, k_m and k_s (wavefunction decay constants) are given in A^(-1)
-    # rho_m and rho_s (metal and semiconductor valence electron densities) are given in A^(-3)
+class Surface :
+    # Constructor for surface of a metal or semiconductor or insulator
+    # - k (wavefunction decay constant) is given in [A^(-1)]
+    # - Sigma (valence electron density per area) are given in [A^(-2)]
     # 
-    # Following convention, C6_m and C6_s are given in [Ha a0^6] instead of [Ha A^6].
-    # Likewise, alpha_m and alpha_s are given in [a0^3] instead of [A^3].
+    # Following convention, 
+    # - C6 is given in [Ha a0^6] instead of [Ha A^6].
+    # - Static atomic polarizability alpha is given in [a0^3] instead of [A^3].
+    #
     # The use of Bohr instead of Angstrom in the vdW-related quantities does not matter because the length unit
     # is divided out when computing the Hamaker constant A_ms.
-    def __init__(self, k_m, k_s, rho_m, rho_s, C6_m, C6_s, alpha_m, alpha_s):
-        self.k_m = k_m                                          # Decay constant in metal [A^(-1)]
-        self.k_s = k_s                                          # Decay constant in semiconductor [A^(-1)]
-        self.sigma_m = rho_m/(2*k_m)                            # Surface charge on metal (no q multiplier) [A^(-2)]
-        self.sigma_s = rho_s/(2*k_s)                            # Surface charge on semiconductor (no q multiplier) [A^(-2)]
-        self.A_ms    = 0.58*np.sqrt(C6_m*C6_s)/alpha_m/alpha_s  # Hamaker constant [Ha]
+    def __init__(self, k, sigma, C6, alpha):
+        self.k     = k
+        self.sigma = sigma
+        self.C6    = C6
+        self.alpha = alpha
 
     # Approximate method to estimate the wavefunction decay constants
     # from the static polarizabilities
-    # Input polarizability "alpha" in Bohr^3.
-    # Output decay constant "k" in A^(-1).
+    #
+    # TODO This model is old and should be deprecated soon.
+    #
+    # - Static polarizability "alpha" in Bohr^3.
+    # - Wavefunction decay constant "k" in A^(-1).
     @staticmethod
     def getDecayFromPolarizability(alpha):
         k = 7.02*np.power(alpha, -0.333)
         return k
 
+    # Assuming the metal wavefunction goes as exp(-kz) outside the metal
+    # Compute the decay constant, k, in terms of the 
+    # - Metal's workfunction [eV]
+    # - Electron effective mass [me]
+    # - Carrier density [A^(-3)]
+    @staticmethod
+    def compMetalDecayConst(W, m, rho):
+        return 0
+
+    # This method creates a metal surface. The inputs are
+    # - Workfunction, W [eV]
+    # - Carrier effective mass, m [me]
+    # - Carrier density, rho [A^(-3)]
+    # - Atomic C6 coefficient, C6 [Ha a0^6]
+    # - Atomic polarizability, alpha [a0^3]
+    @staticmethod
+    def makeMetalSurface(W, m, rho, C6, alpha):
+        k = Surface.compMetalDecayConst(W, m, rho)
+        sigma = rho/(2*k)
+        return Surface(k, sigma, C6, alpha)
+    
+    # This method creates a low-D material surface. The inputs are
+    # - Wavefunction decay constant, k [A^(-1)]
+    # - Valence charge density per area, sigma [A^(-2)]
+    # - Atomic C6 coefficient, C6 [Ha a0^6]
+    # - Atomic polarizability, alpha [a0^3]
+    @staticmethod
+    def make2DSurface(k, sigma, C6, alpha):
+        return Surface(k, sigma, C6, alpha)
+
+
+# Interface class. Generic class which represents metal-semiconductor interfaces.
+# Computes quantities of interest, such as vdW gap distance.
+class Interface :
+    
+    # Constructor for vdW interface between two materials
+    # Pass two Surface objects, s1 and s2, to the constructor.
+    def __init__(self, s1, s2):
+        self.s1 = s1
+        self.s2 = s2
+        self.A_ms = 0.58*np.sqrt(self.s1.C6*self.s2.C6)/(self.s1.alpha*self.s2.alpha)
+
     # Factory method to create an interface, eliminating the dependence on decay constants
     # (which are estimated from the static polarizabilities)
     @staticmethod
-    def makeInterface(rho_m, rho_s, C6_m, C6_s, alpha_m, alpha_s):
-        k_m = Interface.getDecayFromPolarizability(alpha_m)
-        k_s = Interface.getDecayFromPolarizability(alpha_s)
-        return Interface(k_m, k_s, rho_m, rho_s, C6_m, C6_s, alpha_m, alpha_s)
+    def makeInterface(s1, s2):
+        return Interface(s1, s2)
 
     def checkDistSign(self, d):
         if d <= 0 :
@@ -62,10 +98,14 @@ class Interface :
     # Output: energy per area [Ha/A^2]
     def getVDWEnergy(self, d, damp = False):
         self.checkDistSign(d)
+
+        k1 = self.s1.k
+        k2 = self.s2.k
+
         vdw = -self.A_ms/(12*PI*d*d)
         if not damp :
             return vdw
-        damp_dist = 0.2*(1/self.k_m + 1/self.k_s) # TODO what should damp_dist be?
+        damp_dist = 0.2*(1/k1 + 1/k2) # TODO what should damp_dist be?
         damp_rate = 20
         damp_denm = 1 + np.exp(damp_rate*(damp_dist-d))
         return vdw/damp_denm  # Add Fermi function to damp the vdW force
@@ -73,21 +113,29 @@ class Interface :
     # Compute wavefunction overlap at distance d [A]
     # Output wavefunction overlap []
     def getOverlap(self, d):
-        k_avg = 0.5*(self.k_m + self.k_s)
-        S = np.exp(-k_avg*d)*d
-        #S = 0
-        #if self.k_m == self.k_s :
-        #    S = np.exp(-k_avg*d)*d
-        #else :
-        #    S = np.exp(-k_avg*d)*2*np.sinh((self.k_m-self.k_s)*d/2)/(self.k_m-self.k_s)
-        return 2*np.sqrt(self.k_m*self.k_s)*S
+
+        k1 = self.s1.k
+        k2 = self.s2.k
+
+        k_avg = 0.5*(k1 + k2)
+        #S = np.exp(-k_avg*d)*d
+        S = 0
+        if k1 == k2 :
+            S = np.exp(-k_avg*d)*d
+        else :
+            S = np.exp(-k_avg*d)*2*np.sinh((k1-k2)*d/2)/(k1-k2)
+        return 2*np.sqrt(k1*k2)*S
 
     # Compute Pauli repulsion energy at distance d [A]
     # Output: energy per area [Ha/A^2]
     def getPauliEnergy(self, d):
         self.checkDistSign(d)
         S = self.getOverlap(d)
-        return (self.sigma_m + self.sigma_s)*np.sqrt(self.sigma_m*self.sigma_s)*d*S*S/EPS_HA_A_CONST
+
+        sigma1 = self.s1.sigma
+        sigma2 = self.s2.sigma
+
+        return (sigma1 + sigma2)*np.sqrt(sigma1*sigma2)*d*S*S/EPS_HA_A_CONST
     
     # Compute total energy at distance d [A]
     # Output: energy per area [Ha/A^2]
@@ -95,25 +143,19 @@ class Interface :
         self.checkDistSign(d)
         return self.getVDWEnergy(d) + self.getPauliEnergy(d)
     
-    # Compute derivative of energy at distance d [A]
-    # Output: derivative of energy per area [Ha/A^3]
-    def getEnergyDerivative(self, d):
-        d_eps = 0.01
-        return (self.getEnergy(d+0.5*d_eps)-self.getEnergy(d-0.5*d_eps))/d_eps
-    
     # Compute vdW gap (arg min of getEnergy()) and return the value in [A]
     def getVDWGap(self, tol = 1e-3):
 
-        d_curr = 10*(1/self.k_m + 1/self.k_s)  # Select something large (getPauliEnergy() = 0)
-
-        if self.getEnergyDerivative(d_curr) <= 0 :
-            print("ERROR: Energy derivative should be positive for initialized d")
-            quit()
+        k1 = self.s1.k
+        k2 = self.s2.k
+        d_curr = 10*(1/k1 + 1/k2)  # Select something large (getPauliEnergy() = 0)
 
         N = (int)(d_curr/tol)
-        x_vals = np.linspace(d_curr, tol, N)
+        x_vals = np.linspace(d_curr, tol, N)          # Note d_curr >> tol (x_vals is in decreasing order).
         y_vals = [self.getEnergy(x) for x in x_vals]
 
+        # Naive, brute-force method which computes the first minimum obtained 
+        # when iterating over decreasing distance, d.
         i = 1
         while i < N :
             if y_vals[i] < y_vals[i+1] and y_vals[i] < y_vals[i-1] :
@@ -146,76 +188,6 @@ class Interface :
         plt.legend()
 
         title = "Interface energy vs interface separation"
-        if len(extratext) :
-            title += ":\n" + extratext
-        plt.title(title)
-        plt.show()
-
-# AtomAtom class. Generic class which represents vdW interaction between atoms.
-# Computes quantities of interest, such as vdW gap distance.
-#
-# NOTE: in this class, the following units are used:
-# - Length: Angstrom [A]
-# - Energy: Hartree  [Ha]
-#
-# We still use "m" and "s" to refer to the different atoms, although this may
-# not be "metal" or "semiconductor" atoms.
-class AtomAtom(Interface) :
-    
-    # Constructor for metal-semiconductor interface
-    # Here, k_m and k_s (wavefunction decay constants) are given in A^(-1)
-    # rho_m and rho_s (metal and semiconductor valence electron densities) are given in A^(-3)
-    # 
-    # Following convention, C6_m and C6_s are given in [Ha a0^6] instead of [Ha A^6].
-    # Likewise, alpha_m and alpha_s are given in [a0^3] instead of [A^3].
-    # The use of Bohr instead of Angstrom in the vdW-related quantities does not matter because the length unit
-    # is divided out when computing the Hamaker constant A_ms.
-    def __init__(self, k_m, k_s, rho_m, rho_s, C6_m, C6_s, alpha_m, alpha_s):
-        super().__init__(self, k_m, k_s, rho_m, rho_s, C6_m, C6_s, alpha_m, alpha_s)
-
-    # Compute vdW attraction energy at distance d [A]
-    # Output: energy per area [Ha/A^2]
-    def getVDWEnergy(self, d, damp = False):
-        # TODO change this!!
-        self.checkDistSign(d)
-        vdw = -self.A_ms/(12*PI*d*d)
-        if not damp :
-            return vdw
-        damp_dist = 0.2*(1/self.k_m + 1/self.k_s) # TODO what should damp_dist be?
-        damp_rate = 20
-        damp_denm = 1 + np.exp(damp_rate*(damp_dist-d))
-        return vdw/damp_denm  # Add Fermi function to damp the vdW force
-    
-    # Compute Pauli repulsion energy at distance d [A]
-    # Output: energy per area [Ha/A^2]
-    def getPauliEnergy(self, d):
-        # TODO change this!
-        self.checkDistSign(d)
-        k_avg = 0.5*(self.k_m + self.k_s)
-        S = 2*np.sqrt(self.k_m*self.k_s)*d*np.exp(-k_avg*d)
-        return (self.sigma_m + self.sigma_s)*np.sqrt(self.sigma_m*self.sigma_s)*d*S*S/EPS_HA_A_CONST
-    
-    # Plot the interface energy per area as a function of separation
-    def __plotEnergy(self, d_min, d_max):
-        x_vals = np.linspace(d_min, d_max, 100)
-        y_VDW = [self.getVDWEnergy(d, True) for d in x_vals]
-        y_Pau = [self.getPauliEnergy(d) for d in x_vals]
-        y_Tot = [self.getEnergy(d) for d in x_vals]
-        plt.plot(x_vals, y_VDW, color = "black", ls = "--", label = "vdW Energy")
-        plt.plot(x_vals, y_Pau, color = "black", ls = "-.", label = "Pauli Energy")
-        plt.plot(x_vals, y_Tot, color = "black", ls = "-" , label = "Total Energy")
-        plt.xlabel("Inter-atom separation [$\\AA$]")
-        plt.ylabel("Interaction energy [Ha/$\\AA$^2]")
-        return plt
-
-    def plotEnergy(self, extratext = ""):
-        d_VDW = self.getVDWGap()
-        plt = self.__plotEnergy(d_VDW*0.7, d_VDW*2)
-        label = "$d^*=$" + str(round(d_VDW,2)) + " $\\AA$"
-        plt.scatter(d_VDW, self.getEnergy(d_VDW), color = "red", zorder = 100, label = label)
-        plt.legend()
-
-        title = "Atom-atom energy vs inter-atom separation"
         if len(extratext) :
             title += ":\n" + extratext
         plt.title(title)
--- vdW_test.py
+++ vdW_test.py
@@ -1,53 +1,27 @@
-from vdW_distance import Interface
+from vdW_distance import Surface, Interface
 
-def test_one():
-    MS_int = Interface(1,2,0.08,0.005,520,48,558,12)
-    MS_int.plotEnergy()
+def test_graphene_graphene() :
 
-###################
-# Carbon parameters
-###################
+    #####################
+    # Graphene parameters
+    #####################
 
-C6_C    = 47.9  # C6 for Carbon [Ha a0^6]
-alpha_C = 11.7  # Static polarizability for Carbon [a0^3]
+    k_Graphene = 2.3
+    s_Graphene = 0.19    # [A^(-2)] (counts only half an electron per C atom due 
+                         # to pZ-orbital on both sides of graphene sheet)
+    C6_Graphene= 47.9    # [Ha a0^6] (for atomic Carbon)
+    a_Graphene = 11.7    # [a0^3] (for atomic Carbon)
 
-# Graphene has a honeycomb structure with distance 1.42 A between neighboring atoms
-# This implies that each atom occupies 2.62 A^2 area. If Carbon has 4 valence 
-# electrons per atom, the area density of Carbon valence electrons is 4/(2.62 A^2)
-sigma_C = 1.53  # Area density of Carbon valence electrons [A^(-2)]
-#sigma_C  = 0.19
+    ##################
+    # Graphene surface
+    ##################
+    graphene = Surface.make2DSurface(k_Graphene, s_Graphene, C6_Graphene, a_Graphene)
 
-# What is the decay length of graphene wavefunction, perpendicular to the sheet?
-# Not sure but use atomic Carbon as a guide
-#
-# TODO Current issue is that the estimate of atomic size is too large (and therefore k_C is too small)
-#
+    ############################
+    # Graphene-graphee interface
+    ############################
+    gg_interface = Interface.makeInterface(graphene, graphene)
+    gg_interface.plotEnergy("Graphene on Graphene")
 
-k_C     = 3.07              # Inverse decay length of wavefunction perpendicular to graphene sheet [A^(-1)]
-#k_C     = 2.5
-rho_C   = 2*k_C*sigma_C  # Effective volume charge of graphene [e A^(-3)]
+test_graphene_graphene()
 
-# TODO how to convert the energy scale to binding energy in [eV]?
-def test_graphene_graphene():
-    CC_int = Interface(k_C, k_C, rho_C, rho_C, C6_C, C6_C, alpha_C, alpha_C)
-    CC_int.plotEnergy("Graphene-graphene interaction")
-
-def test_carbon_metal(rho_M, C6_M, alpha_M, interfacename = ""):
-    CM_int = Interface.makeInterface(rho_M, rho_C, C6_M, C6_C, alpha_M, alpha_C)
-    CM_int.plotEnergy(interfacename)
-
-############
-# Test bench
-############
-
-test_carbon_metal(rho_C, C6_C, alpha_C, "Carbon on Carbon")
-test_carbon_metal(0.18, 520, 57.5, "Aluminum on Carbon")
-test_carbon_metal(0.059, 427, 45.4, "Gold on Carbon")
-#test_carbon_metal(0.18, 427, 45.4, "Gold on Carbon w/ Aluminum charge density")
-#test_carbon_metal(0.12, 1570, 123, "Scandium on Carbon")
-#test_carbon_metal(0.0847, 264, 41.7, "Copper on Carbon")
-#test_carbon_metal(0.066, 470, 48, "Platinum on Carbon")
-#test_carbon_metal()
-
-#test_graphene_graphene()
-#test_carbon_metal(0.30, 2020, 50.5, "Aluminum on Carbon")

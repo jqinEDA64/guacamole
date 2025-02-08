b517567b4762ee8a1e9e06e53c7d468e2e6173ae
jqinEDA64 <jimmyqin@stanford.edu>
2025-02-08 13:45:52 -0800

    More kernel stuff; write CNL function; clean up CNT raw data (repeated line)

--- guac_math.py
+++ guac_math.py
@@ -14,9 +14,78 @@ import scipy
 import os
 
 
+# Custom definition of "large number"
+FLOAT_MAX = 1e30
+
+
+####################################
+# ERROR HANDLING
+####################################
+
+
+def err_out(msg) :
+    print("ERROR: " + msg)
+    quit()
+
+
 ####################################
 # KERNELS AND KERNEL OPS
 ####################################
+
+
+# Get maximum grid spacing required to
+# have a well-resolved kernel.
+#
+# Inputs:
+# - G : G is the one-dimensional kernel
+#       parameter (either Lorentzian width
+#       or Gaussian standard deviation).
+#       It can be passed either as a scalar
+#       or as a NumPy array. All values must
+#       be positive.
+#
+# Outputs:
+# - dE: Maximum grid spacing required to
+#       resolve all the relevant kernels.
+def getMinResolution(G) :
+    # TODO jqin: reactivate this function once
+    #            resampling is sorted out
+    return 1e5
+    if   isinstance(G, (int, float)):
+        if G == 0 :  # No smoothing; just don't apply any kernel
+            return FLOAT_MAX
+        return G/4.0
+    elif isinstance(G, np.ndarray):
+        return np.min(G)/4.0
+    else:
+        err_out("Input data must be a scalar or a NumPy array.")
+        return 0
+        
+        
+# Returns the current energy resolution, given
+# the energy values. The energy values are
+# assumed to be evenly spaced.
+#
+# Input:
+# - E_vals: Current energy values [eV].
+#
+# Output: 
+# - dE    : Resolution of 1D energy grid [eV].
+def getEnergyResolution(E_vals):
+
+    dE_vals = np.diff(E_vals)
+
+    # Check that E_vals is strictly increasing
+    if not np.all(dE_vals > 0) :
+        err_out("E_vals is not strictly increasing")
+        
+    # Check that E_vals is evenly spaced (to 1% error)
+    if np.ptp(dE_vals) > 0.01*dE_vals[0] :
+        err_out("E_vals is not evenly spaced")
+        
+    # Return spacing
+    dE = np.median(dE_vals)
+    return dE
 
 
 # Returns the half-size of the 
@@ -32,11 +101,14 @@ import os
 # - dE    : grid discretization (float)
 # - Gamma : Lorentzian width    (float)
 def __getLorentzianWidth(Gamma, dE):
+    if dE > getMinResolution(Gamma) :
+        err_out("Insufficient resolution for Lorentzian kernel")
     return (int)(33*Gamma/dE)
+
 
 # Returns the half-size of the
 # Gaussian kernel of standard deviation Sigma,
-# up to 3*Sigma of the integrated content.
+# up to 3*Sigma (99.7%) of the integrated content.
 #
 # If the length of the 1D kernel is 
 # 2N+1, this function returns N+1.
@@ -45,7 +117,10 @@ def __getLorentzianWidth(Gamma, dE):
 # - dE    : grid discretization (float)
 # - Sigma : Gaussian st. dev. (float)
 def __getGaussianWidth(Sigma, dE):
-    return (int)(3*Sigma/dE)
+    if dE > getMinResolution(Sigma) :
+        err_out("Insufficient resolution for Gaussian kernel")
+    return (int)(3*Sigma/dE)+1
+
 
 # Returns the Lorentzian kernel of 
 # width Gamma. 
@@ -70,8 +145,9 @@ def __getLorentzianKernel(Gamma, dE):
     out = scipy.stats.cauchy.pdf(out*scale)
     return out / np.sum(out)  # Normalize
     
+    
 # Returns the Gaussian kernel of 
-# st. dev. Sigma. 
+# standard deviation = Sigma.
 #
 # The length of the 1D kernel is
 # guaranteed to be odd, such that
@@ -103,9 +179,7 @@ def getKernelWidth(Param, dE, kerneltype = "Lorentzian"):
     elif kerneltype == "Gaussian" :
         return __getGaussianWidth  (Param, dE)
     else :
-        print("ERROR: kernel type " + kerneltype + " not recognized")
-        quit()
-        return 0
+        err_out("Kernel type " + kerneltype + " not recognized")
     
     
 # Main function of this section.
@@ -113,15 +187,60 @@ def getKernelWidth(Param, dE, kerneltype = "Lorentzian"):
 # automatically-inferred size), given
 # the kernel parameter and grid spacing
 def getKernel(Param, dE, kerneltype = "Lorentzian"):
+
+    # Return identity if Param = 0
+    # NOTE: requires kernels of ANY type to reduce
+    #       to a delta-function when Param = 0
+    if   Param == 0 :
+        return np.array([1])
+
     if   kerneltype == "Lorentzian" :
         return __getLorentzianKernel(Param, dE)
     elif kerneltype == "Gaussian" :
         return __getGaussianKernel  (Param, dE)
     else :
-        print("ERROR: kernel type " + kerneltype + " not recognized")
-        quit()
+        err_out("Kernel type " + kerneltype + " not recognized")
         return 0
 
+
+# Resampling function to increase the 
+# energy resolution, if needed. Since the
+# function is assumed non-periodic, cannot
+# use Fourier methods. Instead, use 
+# real-space interpolation methods. In this 
+# case, it is not necessary or helpful for
+# the resampling factor "f" to be an integer
+# or inverse of an integer. 
+#
+# Inputs: 
+# - arr_in: Input array to be resampled.
+# - f     : Resampling factor (usually > 1)
+# 
+# Output:
+# - arr_out: Result of resampling.
+def doResample(arr_in, f):
+
+    # TODO jqin: Perhaps, apply appropriate amount of 
+    #            Gaussian pre-smoothing to avoid aliasing
+    #            errors and resample correctly
+
+    # Create cubic spline from input data
+    N_in   = arr_in.shape[0]
+    x_in   = np.arange(N_in)
+    spline = scipy.interpolate.CubicSpline(x_in, arr_in)
+    
+    # Define resampling points
+    N_out = (int)(N_in*f)
+    x_out = np.linspace(0, N_in-1, N_out)
+    
+    # Compute the resampled values by spline interp
+    arr_out = [spline(x) for x in x_out]
+    return np.asarray(arr_out)
+    
+    # Try FFT resampling
+    # Looks horrible for CNT since lots of aliasing
+    #return scipy.signal.resample(arr_in, N_out)
+    
 
 ####################################
 # CONVOLUTIONS AND RELATED OPS
@@ -140,11 +259,9 @@ def __accumIntoArr(arr_1, arr_2, i):
     N1 = arr_1.shape[0]
     N2 = arr_2.shape[0]
     if N2 < N1 :
-        print("ERROR: Array accumulation size mismatch")
-        quit()
+        err_out("Array accumulation size mismatch")
     if (i < 0) or (i+N1 > N2) :
-        print("ERROR: Array accumulation out-of-bounds")
-        quit()
+        err_out("Array accumulation out-of-bounds")
     
     # Perform accumulation op (with NumPy slicing)
     arr_2[i:i+N1] += arr_1
@@ -164,12 +281,14 @@ def __accumIntoArr(arr_1, arr_2, i):
 #              convoluted with nonuniform kernel.
 # - E_in     : Energies of the input array (same as
 #              x-coordinates, essentially). It is assumed
-#              that dE = E_in[i+1]-E_in[i].
+#              that dE = E_in[i+1]-E_in[i], such that the
+#              entries of E_in are evenly spaced.
 # - arr_param: Array of kernel parameters. Must
-#              have same size as arr_in.
+#              have same size as arr_in. Can also be a constant
+#              in which case the same value is used everywhere.
 # - kern_type: Either "Lorentzian" or "Gaussian" (currently)
 #
-# Output:
+# Outputs:
 # - arr_out  : Output array (convoluted version
 #              of arr_in). Is larger than
 #              arr_in due to nonuniform, nonperiodic
@@ -179,19 +298,27 @@ def __accumIntoArr(arr_1, arr_2, i):
 def getNonUnifConv(arr_in, E_in, arr_param, kern_type = "Lorentzian"):
     
     N_in = arr_in.shape[0]
-    if N_in != arr_param.shape[0] :
-        print("ERROR: arr_in and arr_param must have same shape")
     if N_in != E_in.shape[0] :
-        print("ERROR: arr_in and E_in must have same shape")
+        err_out("arr_in and E_in must have same shape")
         
-    # Implicit energy spacing
-    dE = E_in[1] - E_in[0]    
+    # Implicit energy spacing 
+    dE = E_in[1] - E_in[0]
+    
+    # Detect the correct parameter type and create
+    # its array if needed
+    param = None
+    if   isinstance(arr_param, float):
+        param = arr_param*np.ones(N_in)
+    elif isinstance(arr_param, np.ndarray):
+        if N_in != arr_param.shape[0] :
+            err_out("arr_in and arr_param must have same shape")
+        param = arr_param
     
     # Compute the appropriate shape of arr_out
     min_index = 0  # min_index < 0
     max_index = 0  # max_index > N_in
     for i in range(N_in) :
-        N = getKernelWidth(arr_param[i], dE, kern_type)
+        N = getKernelWidth(param[i], dE, kern_type)
         min_index = min(min_index, i-N)
         max_index = max(max_index, i+N)
         
@@ -201,9 +328,9 @@ def getNonUnifConv(arr_in, E_in, arr_param, kern_type = "Lorentzian"):
     
     # Accumulate non-uniform convolution values into output
     for i in range(N_in) :
-        __accumIntoArr(arr_in[i]*getKernel(arr_param[i], dE, kern_type), \
-                       arr_out,                                          \
-                       i - min_index - getKernelWidth(arr_param[i], dE, kern_type))
+        __accumIntoArr(arr_in[i]*getKernel(param[i], dE, kern_type), \
+                       arr_out,                                      \
+                       i - min_index - getKernelWidth(param[i], dE, kern_type))
     
     # Create E_out
     E0    = E_in[0] + min_index*dE  # NOTE: min_index < 0
@@ -212,22 +339,123 @@ def getNonUnifConv(arr_in, E_in, arr_param, kern_type = "Lorentzian"):
     return arr_out, E_out
 
 
+# Charge neutrality level computation.
+# Computes the energy level at which the output has the
+# same amount of charge as the input.
+#
+# Inputs:
+# - D_in   : Density of states of original system.
+# - E_in   : Energy levels of states of original system.
+# - CNL_in : Input charge neutrality level. For semiconductors
+#            CNL_in is not unique (it may be anywhere in the bandgap).
+#            CNL_in does not have to be one of the entries of E_in;
+#            we use interpolation to compute the total charge.
+# - D_out  : Density of states of modified system.
+# - E_out  : Energy levels of states of modified system.
+#
+# Output:
+# - CNL_out: Output charge neutrality level. This better be unique!
+#            If not unique, (i.e., MIGS density is zero), then 
+#            presumably interpolation will fail.
+def getCNL(D_in, E_in, CNL_in, D_out, E_out):
+    
+    # Sanity check that total number of states is preserved
+    Q_tot_in  = np.sum(D_in )
+    Q_tot_out = np.sum(D_out)
+    Q_tot_err = (Q_tot_out-Q_tot_in)/Q_tot_in*100
+    if abs(Q_tot_err) > 0.5 :  # Accept rel. err. less than 0.5%
+        err_out("Inputs to CNL computation do not satisfy Sum Rule")
+    
+    # Compute the functions Q_in(E_in) and Q_out(E_out),
+    # the total charges at zero temperature as a function
+    # of the Fermi level E_{in, out}.
+    Q_in  = scipy.integrate.cumulative_trapezoid(D_in , x = E_in , initial = 0)
+    Q_out = scipy.integrate.cumulative_trapezoid(D_out, x = E_out, initial = 0)
+    
+    # Compute Q(CNL_in), the total amount of electronic charge
+    # in the original ("in") material.
+    Q = scipy.interpolate.CubicSpline(E_in, Q_in)(CNL_in)
+    
+    # Compute CNL_out, the charge neutrality level of the output
+    # density of states. Again use spline interpolation but
+    # "invert" it simply by using E(Q) instead of Q(E).
+    CNL_out = scipy.interpolate.CubicSpline(Q_out, E_out)(Q)
+    
+    return CNL_out
+
+
 ####################################
-# PLOTTING FUNCTIONALITY
+# FILE I/O
 ####################################
 
 # Saves the figure from matplotlib and
-# displays it with native Linux program.
+# displays it with Python viewer.
 #
 # Example of usage:
 #      fig, ax = plt.subplots()
 #      ax.plot(x, y)
 #      ax.set_xlabel("x-axis")
 #      ax.legend()
+#      saveAndShow(fig, "img.png")
 #
-# ... etc.
+# Requires some packages. I think
+# requires Matplotlib, Tkinter, and PyQt6
+# as described here:
+# # https://stackoverflow.com/questions/77507580/userwarning-figurecanvasagg-is-non-interactive-and-thus-cannot-be-shown-plt-sh
+#
 def saveAndShow(fig, fig_path):
     png_path = fig_path + ".png"
     fig.savefig(png_path, dpi=500)
-    os.system("gio open " + png_path + " &")
-    #os.system("imgcat " + png_path + " &")
+    plt.show()
+    
+
+# Load a density of states from
+# file and return the energy and DoS
+# values as NumPy arrays.
+#
+# The DoS gets sorted and the user can
+# also specify a new energy to be defined
+# as the "0" energy reference.
+#
+# Inputs:
+# - file_path: Path to CSV file for DoS.
+# - [E_zero] : Energy (in the original scale)
+#              which is set to zero (in the new scale).
+#
+# Outputs:
+# - E_vals   : Energy values [eV]
+# - D_vals   : Density of states [TODO]
+def loadDoSFromFile(file_path, E_zero = None):
+
+    # Read from disk
+    df = pd.read_csv(file_path)
+    
+    # Check for correct column headers
+    if not "E"   in df.columns :
+        err_out("E not found in DoS CSV file")
+    if not "DOS" in df.columns :
+        err_out("DOS not found in DoS CSV file")
+        
+    # Sort based on E values
+    df = df.sort_values(by = "E") 
+    
+    E_vals = df["E"]
+    D_vals = df["DOS"]
+
+    # Adjust the energy reference if needed
+    if E_zero is not None :
+        E_vals -= E_zero
+
+    # Get median energy spacing
+    dE = getEnergyResolution(E_vals)
+        
+    # Force E_vals to be EXACTLY evenly spaced
+    # Center the extrapolation around E = 0
+    dE_abs = np.abs(np.diff(E_vals)).tolist()
+    i0 = dE_abs.index(min(dE_abs))
+    E0 = E_vals[i0]
+    E_vals = np.asarray([E0 + dE*(i-i0) for i in range(E_vals.shape[0])])
+    
+    return E_vals, D_vals
+
+    
--- guac_test.py
+++ guac_test.py
@@ -2,13 +2,14 @@
 # guac_test.py
 #
 # Testbench for guac_* stuff
-#
 #################################
 
 from guac_math    import *
 from guac_classes import *
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def test_Lorentzian_Kernel(Gamma, dE):
     k = getKernel(Gamma, dE, "Lorentzian")
@@ -60,13 +61,111 @@ def test_NonUnif_Conv_0(Gamma, dE):
     ax.set_title("Test of nonuniform convolution")
     saveAndShow(fig, "test_out/NonUnif_Conv_0")
 
+
+def test_NonUnif_Conv_CNT(Gamma):
+
+    # Read CNT density of states from disk
+    E_vals, D_vals = loadDoSFromFile("raw_data/DoS_CNT_11_0_clean.dat")
+    
+    # Create uniform array of Gamma values
+    G_vals = Gamma*np.ones(E_vals.shape[0])
+    
+    # Compute non-uniform convolution
+    #D_out, E_out = getNonUnifConv(D_vals, E_vals, G_vals, "Lorentzian")
+    D_out, E_out = getNonUnifConv(D_vals, E_vals, G_vals, "Gaussian")
+    
+    # Ensure that input and output satisfy the "Sum Rule"
+    # preserving the total number of states
+    S_in = np.sum(D_vals)
+    S_out= np.sum(D_out )
+    print("Sum Rule Check:")
+    print("  Sum of inputs = " + str(S_in))
+    print("  Sum of outputs= " + str(S_out))
+    print("  Relative error= " + str((S_out-S_in)/S_in*100) + " [%]")
+    
+    # Plot input and output on the same graph
+    fig, ax = plt.subplots()
+    ax.plot(E_vals, D_vals, label = "Original DoS", color = "blue")
+    ax.plot(E_out , D_out , label = "Convoluted DoS", color = "red")
+    ax.legend()
+    ax.set_title("Test of nonuniform convolution")
+    saveAndShow(fig, "test_out/NonUnif_Conv_0")
+
+
+def test_CNL_CNT(Gamma_V, Gamma_C):
+
+    # Read CNT density of states from disk
+    E_vals, D_vals = loadDoSFromFile("raw_data/DoS_CNT_11_0_clean.dat")
+    
+    # TODO jqin: This is just for testing!
+    #            Try to reduce the aliasing error by pre-smoothing
+    D_vals, E_vals = getNonUnifConv(D_vals, E_vals, 0.01, "Gaussian")
+    
+    # Create Gamma values which may be different in the
+    # valence and conduction bands
+    G_vals = np.asarray([Gamma_V if E < 0 else Gamma_C for E in E_vals])
+    
+    # FIRST CASE: constant Gamma
+    #G_vals = 0.03*np.ones(E_vals.shape[0])
+    
+    # SECOND CASE: semimetal broadening
+    #E_Dirac = 0.5
+    #G_vals = 0.03*np.asarray([min(1, np.abs(E-E_Dirac)) for E in E_vals])
+    
+    # Compute non-uniform convolution
+    D_out, E_out = getNonUnifConv(D_vals, E_vals, G_vals, "Lorentzian")
+    #D_out, E_out = getNonUnifConv(D_vals, E_vals, G_vals, "Gaussian")
+    
+    # Ensure that input and output satisfy the "Sum Rule"
+    # preserving the total number of states
+    S_in = np.sum(D_vals)
+    S_out= np.sum(D_out )
+    
+    # Compute the new CNL
+    CNL = getCNL(D_vals, E_vals, 0, D_out, E_out)
+    
+    # Plot input and output on the same graph
+    fig, ax = plt.subplots(2, sharex = True)  # Ensure the plots share a common x-axis
+    fig.suptitle("MIGS and CNL of CNT")
+    ax[0].plot(E_vals, D_vals, label = "Original DoS", color = "blue")
+    ax[0].plot(E_out , D_out , label = "Convoluted DoS", color = "red")
+    ax[0].set_title("Density of states")
+    ax[0].set_ylabel("DoS [eV$^{-1}$ m$^{-1}$]")
+    
+    CNL_label = "CNL = " + str(round(float(CNL), 2)) + " [eV]"
+    ax[0].axvline(x = CNL, label = CNL_label, linestyle = "dashed", color = "black")
+    ax[0].legend()
+    
+    # Also plot Gamma on this graph
+    ax[1].plot(E_vals, G_vals, color = "red")
+    ax[1].set_title("Metal-semiconductor coupling")
+    ax[1].set_xlabel("Energy [eV]")
+    ax[1].set_ylabel("$\\Gamma(E)$ [eV]")
+    saveAndShow(fig, "test_out/CNL_CNT")
+
+
+def test_resample():
+    E_in , D_in  = loadDoSFromFile("raw_data/DoS_CNT_11_0_clean.dat")
+    E_out = doResample(E_in, 4)
+    D_out = doResample(D_in, 4)
+    
+    plt.plot(E_in , D_in , color = "blue")
+    plt.plot(E_out, D_out, color = "red" )
+    plt.title("Test of real-space resampling")
+    plt.show()
+
 #################################
 # TEST BENCH
 #################################
 
-#test_Lorentzian_Kernel(0.5, 0.1)
-#test_Gaussian_Kernel(5, 0.1)
+#test_Lorentzian_Kernel(3, 1)
+#test_Gaussian_Kernel(5, 1)
 
-test_NonUnif_Conv_0(2, 0.1)
+#test_NonUnif_Conv_0(2, 0.1)
+#test_NonUnif_Conv_CNT(0.03)
 
+test_CNL_CNT(0.04, 0.08)
+#test_load_DoS()
+
+#test_resample()
 
